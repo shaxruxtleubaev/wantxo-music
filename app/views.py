84c9754a07fbf555626fddc602d654448246ef43
shaxruxtleubaev <tleubaevshaxrux@outlook.com>
2023-08-23 18:10:30 +0500

    only get request

--- app/views.py
+++ app/views.py
@@ -4,6 +4,6 @@ from .serializers import SongSerializaer
 from rest_framework import viewsets, permissions
 
 class SongViewSet(viewsets.ModelViewSet):
-    permission_classes = (permissions.AllowAny,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     queryset = Song.objects.all()
     serializer_class = SongSerializaer
